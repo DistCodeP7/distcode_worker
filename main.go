12b4086cd171b50b9181c3d1e5528e63225f7cc2
Andreas Mertz <Andreasmertz11@gmail.com>
2025-09-15 12:34:29 +0200

    This code correctly uses the Dodcker Package to list local images, this illustrates how we can use the package to communicate and handle errors

--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
 )
 
-
 func main() {
-	fmt.Println("Hello, World!")
+    ctx := context.Background()
+
+    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+    if err != nil {
+        log.Fatalf("Error creating Docker client: %v", err)
+    }
+
+    // Use the correct ListOptions from image package
+    images, err := cli.ImageList(ctx, image.ListOptions{})
+    if err != nil {
+        log.Fatalf("Error listing images: %v", err)
+    }
+
+    if len(images) == 0 {
+        fmt.Println("No Docker images found locally.")
+        return
+    }
+
+    fmt.Println("Local Docker images:")
+    for _, img := range images {
+        tags := img.RepoTags
+        if len(tags) == 0 {
+            tags = []string{"<none>:<none>"}
+        }
+        for _, tag := range tags {
+            fmt.Printf(" - %s (ID: %.12s) Size: %.2f MB\n", tag, img.ID, float64(img.Size)/(1024*1024))
+        }
+    }
 }
