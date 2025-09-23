c298575827380a1c438029164016bbb387e1e596
simo205j <113094975+Simo205j@users.noreply.github.com>
2025-09-23 10:40:56 +0200

    Refractor of the reciever.go to include handleDelievery function. Addtionally added reciever_test.go to test the new function

---
+++ reciever/reciever_test.go
@@ -0,0 +1,66 @@
+package reciever
+
+import (
+	"testing"
+
+	"github.com/DistCodeP7/distcode_worker/types"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func TestHandleDelivery_ValidJSON(t *testing.T) {
+	jobs := make(chan types.JobRequest, 1)
+
+	d := amqp.Delivery{Body: []byte(`{"ProblemId": 42}`)}
+	handleDelivery(d, jobs)
+
+	select {
+	case job := <-jobs:
+		if job.ProblemId != 42 {
+			t.Errorf("expected ProblemId=42, got %d", job.ProblemId)
+		}
+	default:
+		t.Error("expected a job to be sent to channel, got none")
+	}
+}
+
+func TestHandleDelivery_InvalidJSON(t *testing.T) {
+	jobs := make(chan types.JobRequest, 1)
+
+	d := amqp.Delivery{Body: []byte(`not json`)}
+	handleDelivery(d, jobs)
+
+	select {
+	case <-jobs:
+		t.Error("expected no job for invalid JSON, but got one")
+	default:
+		// pass
+	}
+}
+
+func TestHandleDelivery_NilBody(t *testing.T) {
+	jobs := make(chan types.JobRequest, 1)
+
+	d := amqp.Delivery{Body: nil}
+	handleDelivery(d, jobs)
+
+	select {
+	case <-jobs:
+		t.Error("expected no job for nil body, but got one")
+	default:
+		// pass
+	}
+}
+
+func TestHandleDelivery_EmptyBody(t *testing.T) {
+	jobs := make(chan types.JobRequest, 1)
+
+	d := amqp.Delivery{Body: []byte(``)}
+	handleDelivery(d, jobs)
+
+	select {
+	case <-jobs:
+		t.Error("expected no job for empty body, but got one")
+	default:
+		// pass
+	}
+}
